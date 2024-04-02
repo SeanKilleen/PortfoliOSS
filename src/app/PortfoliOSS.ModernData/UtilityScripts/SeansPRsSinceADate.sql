dd4bf1a884071855da3d5fe36378a93893b21ee5
Sean Killeen <SeanKilleen@gmail.com>
2024-04-02 09:28:44 -0400

    Add link to query

--- src/app/PortfoliOSS.ModernData/UtilityScripts/SeansPRsSinceADate.sql
+++ src/app/PortfoliOSS.ModernData/UtilityScripts/SeansPRsSinceADate.sql
@@ -6,7 +6,7 @@
     and IsMerged = 1
     and MergedDate >= '2023-03-01'
 
-Select orgs.Name OrgName, repos.Name RepoName, MergedDate from PullRequests prs
+Select orgs.Name OrgName, repos.Name RepoName, MergedDate, CONCAT('https://github.com/', orgs.Name, '/', repos.Name, '/pull/', prs.PullRequestId) as Link from PullRequests prs
     left outer join Repositories repos on (prs.RepositoryRepoId = repos.RepoId)
     left outer join Organizations orgs on (repos.OrganizationId = orgs.OrganizationId)
     where AuthorUserId = (Select UserId from Users where Name ='SeanKilleen') 
